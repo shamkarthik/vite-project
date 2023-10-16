b5b4b53e1c1a1c97bbc11dce1d7ee5166f084460
ShamKarthikS <ShamKarthik88@gmail.com>
2023-10-16 20:27:02 +0530

    vercel analytics ver 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { inject } from "@vercel/analytics"
+import { Analytics } from "@vercel/analytics/react"
 import { RouterProvider, createHashRouter } from "react-router-dom"
 import { ThemeProvider } from "./context/ThemeContext"
 import { routes } from "./routes"
 
 function App() {
-  inject()
   const router = createHashRouter(routes)
   return (
     // <Provider store={store}>
     <ThemeProvider>
       <RouterProvider router={router} />
+      <Analytics />
     </ThemeProvider>
     // </Provider>
   )
